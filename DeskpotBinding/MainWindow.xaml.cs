69927b5655d2203003d735370c1fd966f5373a8f
Mehdi_ha01 <Mehdi_ha01@STHQ0127-11>
2023-03-28 13:35:37 +0400

    Add project files.

---
+++ DeskpotBinding/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DeskpotBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (same.IsChecked == true)
+            {
+                System.Windows.Data.Binding binding = new System.Windows.Data.Binding();
+                binding.Path = new PropertyPath("Value");
+                binding.Mode = BindingMode.Default;
+
+                binding.Source = slider1;
+                slider2.SetBinding(Slider.ValueProperty, binding);
+            }
+            else if (same.IsChecked == false)
+            {
+
+                BindingOperations.ClearBinding(slider2, Slider.ValueProperty);
+                slider2.Value = slider1.Value;
+            }
+        }
+    }
+}
